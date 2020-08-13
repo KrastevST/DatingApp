f63795acdd663a8c126ac780c0a4279d126ca464
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-08-13 17:25:35 +0300

    added Register in AuthController

---
+++ DatingApp.API/Controllers/AuthControler.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+  [Route ("api/[controller]")]
+  [ApiController]
+  public class AuthControler : ControllerBase
+  {
+    private readonly IAuthRepository repo;
+    public AuthControler (IAuthRepository repo)
+    {
+      this.repo = repo;
+
+    }
+
+    [HttpPost ("register")]
+    public async Task<IActionResult> Register (string username, string password)
+    {
+      // TODO: validate request
+
+      username = username.ToLower ();
+
+      if (await this.repo.UserExists (username))
+        return BadRequest ("Username already exists");
+
+      var userToCreate = new User
+      {
+        Username = username
+      };
+
+      var createdUser = await this.repo.Register(userToCreate, password);
+
+      return StatusCode(201);
+    }
+  }
+}
