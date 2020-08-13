7b5624aef1c9030dbcc08d5329817ac741065286
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-08-13 14:51:54 +0300

    added AuthRepository with register mrthod

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -8,50 +8,47 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
+  [Route ("api/[controller]")]
+  [ApiController]
+  public class ValuesController : ControllerBase
+  {
+    private readonly DataContext context;
+    public ValuesController (DataContext context)
     {
-        private readonly DataContext context;
-          public ValuesController(DataContext context)
-          {
-            this.context = context;
+      this.context = context;
 
-        }
-        // GET api/values
-        [HttpGet]
-        public async Task<IActionResult> GetValues()
-        {
-            var values = await this.context.Values.ToListAsync();
-
-          return Ok(values);
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetValue(int id)
-        {
-          var value = await this.context.Values.FirstOrDefaultAsync(x => x.Id == id);
-
-          return Ok(value);
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
+    // GET api/values
+    [HttpGet]
+    public async Task<IActionResult> GetValues ()
+    {
+      var values = await this.context.Values.ToListAsync ();
+
+      return Ok (values);
+    }
+
+    // GET api/values/5
+    [HttpGet ("{id}")]
+    public async Task<IActionResult> GetValue (int id)
+    {
+      var value = await this.context.Values.FirstOrDefaultAsync (x => x.Id == id);
+
+      return Ok (value);
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post ([FromBody] string value)
+    {}
+
+    // PUT api/values/5
+    [HttpPut ("{id}")]
+    public void Put (int id, [FromBody] string value)
+    {}
+
+    // DELETE api/values/5
+    [HttpDelete ("{id}")]
+    public void Delete (int id)
+    {}
+  }
 }
--- DatingApp.API/Migrations/DataContextModelSnapshot.cs
+++ DatingApp.API/Migrations/DataContextModelSnapshot.cs
@@ -7,49 +7,49 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp.API.Migrations
 {
-    [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+  [DbContext (typeof (DataContext))]
+  partial class DataContextModelSnapshot : ModelSnapshot
+  {
+    protected override void BuildModel (ModelBuilder modelBuilder)
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.6");
+      modelBuilder
+        .HasAnnotation ("ProductVersion", "3.1.6");
 
-            modelBuilder.Entity("DatingApp.API.Models.User", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+      modelBuilder.Entity ("DatingApp.API.Models.User", b =>
+      {
+        b.Property<int> ("Id")
+          .ValueGeneratedOnAdd ()
+          .HasColumnType ("INTEGER");
 
-                    b.Property<byte[]>("PasswordHash")
-                        .HasColumnType("BLOB");
+        b.Property<byte[]> ("PasswordHash")
+          .HasColumnType ("BLOB");
 
-                    b.Property<byte[]>("PasswordSalt")
-                        .HasColumnType("BLOB");
+        b.Property<byte[]> ("PasswordSalt")
+          .HasColumnType ("BLOB");
 
-                    b.Property<string>("Username")
-                        .HasColumnType("TEXT");
+        b.Property<string> ("Username")
+          .HasColumnType ("TEXT");
 
-                    b.HasKey("Id");
+        b.HasKey ("Id");
 
-                    b.ToTable("Users");
-                });
+        b.ToTable ("Users");
+      });
 
-            modelBuilder.Entity("DatingApp.API.Models.Value", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+      modelBuilder.Entity ("DatingApp.API.Models.Value", b =>
+      {
+        b.Property<int> ("Id")
+          .ValueGeneratedOnAdd ()
+          .HasColumnType ("INTEGER");
 
-                    b.Property<string>("Name")
-                        .HasColumnType("TEXT");
+        b.Property<string> ("Name")
+          .HasColumnType ("TEXT");
 
-                    b.HasKey("Id");
+        b.HasKey ("Id");
 
-                    b.ToTable("Values");
-                });
+        b.ToTable ("Values");
+      });
 #pragma warning restore 612, 618
-        }
     }
+  }
 }
